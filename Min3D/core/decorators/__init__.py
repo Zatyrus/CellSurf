ae0eadebcacb14f14d502d0b5216586ae4d56369
Bela Vogler <bela.vogler@uni-jena.de>
2026-06-12 21:08:19 +0200

    clean
    ~ removed "auto_checkpoint"
    ~ removed "auto_log"
    
    it was all a little overkill

--- Min3D/core/decorators/__init__.py
+++
@@ -1,5 +0,0 @@
-from Min3D.core.decorators.auto_log import auto_log
-from Min3D.core.decorators.auto_checkpoint import auto_checkpoint
-
-# namespace package
-__all__ = ["auto_log", "auto_checkpoint"]
