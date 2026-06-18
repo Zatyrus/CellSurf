f0976f2e405bf60b1a5f798b51c323634f1434f0
Bela Vogler <bela.vogler@uni-jena.de>
2026-06-18 18:10:37 +0200

    Update shortest_path_interactive.py
    ~ introduce draw_geometries to the interactive shortest path visualizer

--- cellnav/visualization/shortest_path_interactive.py
+++ cellnav/visualization/shortest_path_interactive.py
@@ -3,23 +3,28 @@ import numpy as np
 import open3d as o3d
 from copy import copy
 
+from typing import Union
+
 
 ## custom dependencies
 from cellnav.core.framework.surface_graph import SurfaceGraph
 from cellnav.core.helpers.geo_shape_helper import GeoShapeHelper
 from cellnav.core.helpers.estimate_magnitude_from_data import estimate_magnitude_from_data
+from cellnav.core.helpers.draw_geometries import draw_geometries
 
 __all__ = ["shortest_path_interactive"]
 
 
 # %% Visualization functions
-def shortest_path_interactive(graph: SurfaceGraph, source: int, target: int) -> None:
+def shortest_path_interactive(graph: SurfaceGraph, source: int, target: int, scalebar: Union[float, int, bool] = False, size: Union[float, int] = 1.0) -> None:
     """Spin up an interactive visualization of the shortest path between source and target nodes in the given surface graph.
 
     Args:
         graph (SurfaceGraph): The surface graph containing the nodes and edges.
         source (int): The node from which to start the path.
         target (int): The node at which to end the path.
+        scalebar (Union[float, int, bool]): The size of the scale bar or coordinate frame (3D scalebar). No scalebar will be added if set to False. Defaults to False.
+        size (Union[float, int]): The size of the visualization. Defaults to 1.0.
 
     Returns:
         None: This function does not return anything. It opens an interactive visualization window.
@@ -29,7 +34,7 @@ def shortest_path_interactive(graph: SurfaceGraph, source: int, target: int) -> 
     # select base wireframe for visualization
     wireframe = copy(graph.edges)
     path = graph.get_shortest_path(source, target)
-    approximate_scaler = np.power(10, estimate_magnitude_from_data(graph.vertices.get_points()) - 2)
+    approximate_scaler = np.power(10, estimate_magnitude_from_data(graph.vertices.get_points()) - 1)
 
     # create lineset for the path
     path_edges = [(path[i], path[i + 1]) for i in range(len(path) - 1)]
@@ -39,17 +44,17 @@ def shortest_path_interactive(graph: SurfaceGraph, source: int, target: int) -> 
 
     # create spheres for the start and end nodes
     start_sphere = GeoShapeHelper.generate_sphere_on_point(
-        wireframe.points[source], radius=3 * approximate_scaler, color=[0, 1, 0]
+        wireframe.points[source], radius=size * approximate_scaler, color=[0, 1, 0]
     )  # green for start node
     end_sphere = GeoShapeHelper.generate_sphere_on_point(
-        wireframe.points[target], radius=3 * approximate_scaler, color=[0, 0, 1]
+        wireframe.points[target], radius=size * approximate_scaler, color=[0, 0, 1]
     )  # blue for end node
 
     # add smaller spheres for the intermediate nodes in the path
     intermediate_nodes = []
     for node in path[1:-1]:
         sphere = GeoShapeHelper.generate_sphere_on_point(
-            wireframe.points[node], radius=2 * approximate_scaler, color=[1, 0, 0]
+            wireframe.points[node], radius=size * 0.75 * approximate_scaler, color=[1, 0, 0]
         )  # red for intermediate nodes
         intermediate_nodes.append(sphere)
 
@@ -62,8 +67,9 @@ def shortest_path_interactive(graph: SurfaceGraph, source: int, target: int) -> 
     )
 
     # visualize the base wireframe and the path lineset together
-    o3d.visualization.draw_geometries(  # type: ignore
+    draw_geometries(
         [path_lineset, wireframe.geometry, start_sphere, end_sphere]
         + intermediate_nodes,
-        window_name=f"Shortest Path from Node {source} to Node {target}",
+        scalebar=scalebar,
+        window_name=f"Shortest Path from Node {source} to Node {target}"
     )
