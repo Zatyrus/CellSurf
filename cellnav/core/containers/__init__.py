70db3c69e77295a7e71c7d8499af4954c4441743
Bela Vogler <bela.vogler@uni-jena.de>
2026-06-22 17:55:23 +0200

    Update __init__.py
    Add Path3D to import namespace of core.containers

--- cellnav/core/containers/__init__.py
+++ cellnav/core/containers/__init__.py
@@ -2,6 +2,9 @@
 from cellnav.core.containers.geometry_base import (
     GeometryBase as GeometryBase,
 )
+from cellnav.core.containers.path_3d import (
+    Path3D as Path3D,
+)
 
 
-__all__ = ["GeometryBase"]
+__all__ = ["GeometryBase", "Path3D"]
